f59e8eda31471e03be339afa4fb4b094983ad2a2
ywmaa <yousefabas01000@gmail.com>
2021-11-08 22:14:12 +0200

    Fixed a bug in interaction system

--- Player/Interaction.gd
+++ Player/Interaction.gd
@@ -7,7 +7,7 @@ var current_collider
 func _ready():
 	interaction_label.set_text("")
 
-func _process():
+func _process(_delta):
 	var collider = get_collider()
 	
 	if is_colliding() and collider is Interactable:
