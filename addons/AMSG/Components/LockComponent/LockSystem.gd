3f7e55a010debb11cb5b673f125d522afd190b46
Adam Courtney <adam@thefunnybrain.com>
2023-09-17 02:06:12 +0100

    Removed the Pause Attribute and made state change inherently part of the lock system - now uses the wonderful Signal system! :) Also repathed the project.godot icon to prevent an error, and added a few debug functions to LockSystem

--- addons/AMSG/Components/LockComponent/LockSystem.gd
+++ addons/AMSG/Components/LockComponent/LockSystem.gd
@@ -11,6 +11,10 @@ var lock_count : int :
 
 #An event to hook into - primarily for debugging.
 signal Lock_Added(lockName:String)
+signal Lock_Removed(lockName:String)
+
+#This one should only emit if is_locked would have changed.
+signal Lock_Status_Changed(newStatus:bool)	
 
 #A getter to see if any locks are being applied
 @export var is_locked : bool :
@@ -19,18 +23,26 @@ signal Lock_Added(lockName:String)
 
 #If a lock called lock_name hasn't already been added, adds one.
 func add_lock(lock_name:String):
+	#Don't add duplicate locks
 	if(contains_lock(lock_name)):
 		print_debug("Lock %lock is already added." % lock_name)
 		return
 	else:
+		#Add locks and emit events
 		_locks.append(lock_name)
 		emit_signal("Lock_Added", lock_name)
+		#if this is the first and only lock, the locked status has changed to true
+		if(_locks.size() == 1):
+			Lock_Status_Changed.emit(true)
 		return;
 		
 #Removes a lock with the name lock_name. Prints a message if it's not in there.
 func remove_lock(lock_name:String):
 	if(contains_lock(lock_name)):
 		_locks.erase(lock_name)
+		#If there's now zero locks remaining, emit event
+		if(_locks.size() == 0):
+			Lock_Status_Changed.emit(false)
 	else:
 		print_debug("Lock %lock cannot be removed as it isn't there." % lock_name)
 
@@ -44,3 +56,15 @@ func contains_lock(lock_name:String):
 		if lock == lock_name:
 			return true;
 	return false;
+
+#Prints all current locks - useful for tracking down issues when locks haven't been lifted
+func debug_locks():
+	var log = "Printing all locks"
+	for lock in _locks:
+		print_debug("\n" + str(lock))
+
+#To be used for debug - for when the locks need to be bypassed to test.
+func debug_release_all_locks():
+	for lock in _locks:
+		Lock_Removed.emit(lock)
+	_locks.clear();
